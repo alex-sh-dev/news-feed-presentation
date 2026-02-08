055d3ea97716fd48fa3ffbed895fd415c5be70f7
dev <->
2026-02-08 11:55:43 +0500

    feat: - created new project named NewsFeedPresentaion - added .gitignore file - configured LaunchScreen - etc

---
+++ NewsFeedPresentation/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  NewsFeedPresentation
+//
+//  Created by dev on 2/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

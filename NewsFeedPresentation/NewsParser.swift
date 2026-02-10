8e83d08ee5b83eca437bb9d7880aa5dbdb4bbb92
dev <->
2026-02-10 12:40:47 +0500

    feat: added parser to parse news from json data (NewsParser - singleton)

---
+++ NewsFeedPresentation/NewsParser.swift
@@ -0,0 +1,67 @@
+//
+//  NewsParser.swift
+//  NewsFeedPresentation
+//
+//  Created by dev on 2/9/26.
+//
+
+import Foundation
+
+class NewsParser {
+    
+    static let shared = NewsParser()
+    
+    struct Config {
+        let baseEndpoint: URL
+    }
+    
+    private static var config: Config?
+    var baseEndpoint: URL
+    
+    private init() {
+        guard let config = NewsParser.config else {
+            fatalError("Error: you must call setup before accessing NewsParser.shared")
+        }
+        
+        self.baseEndpoint = config.baseEndpoint
+    }
+    
+    class func setup(_ config: Config) {
+        NewsParser.config = config
+    }
+    
+    func sendRequest(page: UInt, count: UInt) {
+        //?? page * count <= totalCount
+        //?? sequentially? DispatchQueue
+        
+        let endpoint = self.baseEndpoint
+            .appending(path: String(page))
+            .appending(path: String(count))
+        
+        
+        URLSession.shared.dataTask(with: endpoint) {
+            (data, response, error) -> Void in
+            
+            if error == nil && data != nil {
+                do {
+                    let result = try JSONDecoder().decode(NewsNode.self, from: data!)
+                    guard let news = result.news else {
+                        //publish error? //??
+                        return //??
+                    }
+                    
+                    for newsItem in news {
+                        NewsStorage.shared.lock.with() {
+                            NewsStorage.shared.news[newsItem.id] = newsItem
+                        }
+                    }
+                } catch {
+                    //publish error? //??
+                    print(error) //??
+                }
+            } else {
+                //publish error? //??
+            }
+        }.resume()
+    }
+}

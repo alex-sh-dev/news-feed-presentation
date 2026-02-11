9ce8dda81cb704f073e6dc43ebf1635946f7d069
dev <->
2026-02-11 16:03:55 +0500

    feat: URLSession.shared.dataTask -> URLSession.shared.dataTaskPublisher (publisher usage)

--- Source/Basic/NewsParser.swift
+++ Source/Basic/NewsParser.swift
@@ -9,7 +9,6 @@ import Foundation
 import Combine
 
 class NewsParser {
-    
     static let shared = NewsParser()
     
     struct Config {
@@ -18,7 +17,9 @@ class NewsParser {
     
     private static var config: Config?
     var baseEndpoint: URL
-    let idsPub = PassthroughSubject<[UInt], Never>() //?? rename?
+    
+    private var cancellable = [String: AnyCancellable]()
+    let newsUpdatedPublisher = PassthroughSubject<Bool, Never>()
     
     private init() {
         guard let config = NewsParser.config else {
@@ -32,40 +33,36 @@ class NewsParser {
         NewsParser.config = config
     }
     
-    func sendRequest(page: UInt, count: UInt) {
-        //?? page * count <= totalCount
-        
+    func requestData(page: UInt = 1, count: UInt) {
         let endpoint = self.baseEndpoint
             .appending(path: String(page))
             .appending(path: String(count))
         
-        
-        URLSession.shared.dataTask(with: endpoint) {
-            [unowned self] (data, response, error) -> Void in
-            
-            if error == nil && data != nil {
-                do {
-                    let result = try JSONDecoder().decode(NewsNode.self, from: data!)
-                    guard let news = result.news else {
-                        //publish error? //??
-                        return //??
-                    }
-                    
-                    var ids = [UInt]()
-                    for newsItem in news {
-                        NewsStorage.shared.lock.with {
-                            NewsStorage.shared.news[newsItem.id] = newsItem
-                        }
-                        ids.append(newsItem.id)
-                    }
-                    self.idsPub.send(ids)
-                } catch {
-                    //publish error? //??
-                    print(error) //??
+        let uuid = UUID().uuidString
+        let cancellable = URLSession.shared.dataTaskPublisher(for: endpoint)
+            .map { $0.data }
+            .retry(3)
+            .decode(type: NewsNode.self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .replaceError(with: NewsNode.zero)
+            .eraseToAnyPublisher()
+            .sink(receiveValue: {
+                [weak self] result in
+                guard let news = result.news, !news.isEmpty else {
+                    self?.newsUpdatedPublisher.send(false)
+                    return
                 }
-            } else {
-                //publish error? //??
-            }
-        }.resume()
+                
+                var ids = [UInt]()
+                for newsItem in news {
+                    NewsStorage.shared.lock.with {
+                        NewsStorage.shared.news[newsItem.id] = newsItem
+                    }
+                    ids.append(newsItem.id)
+                }
+                self?.newsUpdatedPublisher.send(true)
+                self?.cancellable.removeValue(forKey: uuid)
+            })
+        self.cancellable[uuid] = cancellable
     }
 }

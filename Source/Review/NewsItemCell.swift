f0441e7f8df97f672da5f5cfebd6ca6170af7f3b
dev <->
2026-02-14 22:20:34 +0500

    feat: added some logic for showInFullButton

--- Source/Review/NewsItemCell.swift
+++ Source/Review/NewsItemCell.swift
@@ -12,6 +12,25 @@ class NewsItemCell: UICollectionViewCell {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var categoryLabel: InsetLabel!
+    @IBOutlet weak var showInFullButton: UIButton!
+    @IBOutlet weak var showInFullHeightConstraint: NSLayoutConstraint! {
+        didSet {
+             savedHeightConstant = self.showInFullHeightConstraint.constant
+        }
+    }
+    
+    private var savedHeightConstant: CGFloat = 0
     
     static let identifier = "NewsItemCellIdentifier"
+    
+    var showInFullTappedHandler: (() -> Void)? = nil
+    
+    @IBAction func showInFullTapped(_ sender: Any) {
+        self.showInFullTappedHandler?()
+    }
+    
+    func hideShowInFullButton(_ hidden: Bool) {
+        self.showInFullButton.isHidden = hidden
+        self.showInFullHeightConstraint.constant = hidden ? 0 : self.savedHeightConstant
+    }
 }

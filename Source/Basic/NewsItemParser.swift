2835c38ea9cadc00b59605c98f496ffb9a6084a7
dev <->
2026-02-19 15:56:50 +0500

    feat: added parser to parse full text, urls of images of news item using attributed string

---
+++ Source/Basic/NewsItemParser.swift
@@ -0,0 +1,64 @@
+//
+//  NewsItemParser.swift
+//  NewsFeedPresentation
+//
+//  Created by dev on 2/19/26.
+//
+
+import Foundation
+
+class NewsItemParser {
+    private(set) var texts: [String] = []
+    private(set) var imageUrls: [URL] = []
+    private let text: String!
+    
+    var finalText: String {
+        if self.texts.isEmpty {
+            return ""
+        }
+
+        return self.texts.joined(separator: "")
+    }
+    
+    init(text: String) {
+        self.text = text
+    }
+    
+    func imageUrls(withExcludedUrl url: URL) -> [URL] {
+        if self.imageUrls.isEmpty {
+            return []
+        }
+        
+        self.imageUrls.removeAll { $0 == url }
+        return self.imageUrls
+    }
+    
+    func parse() {
+        guard let data = text.data(using: String.Encoding.utf16,
+                                   allowLossyConversion: false) else {
+            return
+        }
+        
+        let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [.documentType: NSAttributedString.DocumentType.html]
+        guard let attrString = try? NSMutableAttributedString(data: data, options: options, documentAttributes: nil) else {
+            return
+        }
+        
+        attrString.enumerateAttributes(in: NSRange(location: 0, length: attrString.length), options: []) {
+            (attributes, range, stop) in
+            let substring = (attrString.string as NSString).substring(with: range)
+            let isPresent = !substring.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+            if isPresent && (attributes[.link] == nil || attributes[.attachment] == nil) {
+                self.texts.append(substring)
+            }
+        }
+            
+        attrString.enumerateAttribute(.link, in: NSRange(location: 0, length: attrString.length), options: []) {
+            (value, range, stop) in
+            if let url = value as? URL,
+               !url.pathExtension.isEmpty {
+                imageUrls.append(url)
+            }
+        }
+    }
+}

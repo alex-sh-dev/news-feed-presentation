e5509cf113e3da7f9784a0b2a133b5191bd6bef6
dev <->
2026-02-19 20:02:08 +0500

    feat: improved NewsItemParser (added serial parsing of news items)

--- Source/Basic/NewsItemParser.swift
+++ Source/Basic/NewsItemParser.swift
@@ -10,70 +10,69 @@ import Combine
 
 class NewsItemParser {
     private let config: WebConfig!
-    
-    private var cancellable = [String: AnyCancellable]()
     private let operationSerialQueue = AsyncOperationQueue()
     
-    let imageUrlsUpdatedPublisher = PassthroughSubject<UInt, Never>()
-    let newsItemTextUpdatedPublisher = PassthroughSubject<UInt, Never>()
+    let newsItemUpdatedPublisher = PassthroughSubject<UInt, Never>()
     
     init(config: WebConfig) {
         self.config = config
     }
     
-    private func addParseTask(_ parseTask: NewsItemParseTask) {
+    private func startParseTask(_ parseTask: NewsItemParseTask) {
+        guard let id = parseTask.id else {
+            return
+        }
+
+        parseTask.start()
+
+        let storage = NewsStorage.shared
+        storage.lock.with {
+            storage.fullTextContents[id] = parseTask.finalText
+        }
+
+        storage.lock.with {
+            if let urls = parseTask.finalImageUrls {
+                storage.imageUrls[id] = urls
+            }
+        }
+
+        self.newsItemUpdatedPublisher.send(id)
+        easyLog("news item with id = \(id) parsed")
+    }
+    
+    private func request(for url: URL, with id: UInt) async throws {
+        let (data, _) = try await URLSession.shared.data(from: url)
+        if data.isEmpty {
+            return
+        }
+        
+        let result = try JSONDecoder().decode(TextNewsItem.self, from: data)
+        guard let text = result.text else {
+            return
+        }
+
+        let parseTask = NewsItemParseTask(text: text, titleImageUrl: result.titleImageUrl, for: id)
+        self.startParseTask(parseTask)
+    }
+
+    func requestNewsItem(subUrl: URL, for id: UInt) {
+        let endpoint = self.config.newsItemEndpoint!
+            .appending(path: subUrl.absoluteString)
+        
         self.operationSerialQueue.enqueue { [weak self] in
             guard let self = self else { return }
-            
-            guard let id = parseTask.id else {
-                return
-            }
-            
-            parseTask.start()
-            
-            let storage = NewsStorage.shared
-            storage.lock.with {
-                storage.fullTextContents[id] = parseTask.finalText
-                self.newsItemTextUpdatedPublisher.send(id)
-            }
-            
-            storage.lock.with {
-                if let urls = parseTask.finalImageUrls {
-                    storage.imageUrls[id] = urls
-                    self.imageUrlsUpdatedPublisher.send(id)
+            let maxRetries = self.config.requestAttemptsCount
+            let delay = self.config.sendRequestDelayMs * NSEC_PER_MSEC
+            for _ in 0..<maxRetries {
+                do {
+                    try await self.request(for: endpoint, with: id)
+                    break
+                } catch {
+                    do {
+                        try await Task.sleep(nanoseconds: delay)
+                    } catch {}
                 }
             }
         }
     }
-    
-//    NewsImageUrlsExtractor.shared.addTask(for: newsItem.titleImageUrl, with: newsItem.id)//??
-    
-    func requestNewsItem(subUrl: URL, for id: UInt) {
-        let endpoint = self.config.newsItemEndpoint!
-            .appending(path: subUrl.absoluteString)
-            
-        let uuid = UUID().uuidString
-        let cancellable = URLSession.shared.dataTaskPublisher(for: endpoint)
-            .map { $0.data }
-            .retry(self.config.requestAttemptsCount)
-            .decode(type: TextNewsItem.self, decoder: JSONDecoder())
-            .receive(on: DispatchQueue.main)
-            .eraseToAnyPublisher()
-            .sink(receiveCompletion: { _ in
-            }, receiveValue: { [weak self] result in
-                guard let text = result.text else {
-                    return
-                }
-                let parseTask = NewsItemParseTask(text: text, titleImageUrl: result.titleImageUrl, for: id)
-                self?.addParseTask(parseTask)
-                DispatchQueue.main.async { [weak self] in
-                    self?.cancellable.removeValue(forKey: uuid)
-                }
-            })
-        self.cancellable[uuid] = cancellable
-    }
-    
-   
 }
-
-

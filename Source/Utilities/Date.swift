5c26acc5c4bab965a716f9b6568315aa1789504b
dev <->
2026-03-09 16:12:20 +0500

    refactor: relativeDateFormatter -> formatter

--- Source/Utilities/Date.swift
+++ Source/Utilities/Date.swift
@@ -10,10 +10,10 @@ import Foundation
 extension Date {
     func relativeDate(timeStyle: DateFormatter.Style = .none,
                       dateStyle: DateFormatter.Style = .full) -> String {
-        let relativeDateFormatter = DateFormatter()
-        relativeDateFormatter.timeStyle = timeStyle
-        relativeDateFormatter.dateStyle = dateStyle
-        relativeDateFormatter.doesRelativeDateFormatting = true
-        return relativeDateFormatter.string(from: self)
+        let formatter = DateFormatter()
+        formatter.timeStyle = timeStyle
+        formatter.dateStyle = dateStyle
+        formatter.doesRelativeDateFormatting = true
+        return formatter.string(from: self)
     }
 }
